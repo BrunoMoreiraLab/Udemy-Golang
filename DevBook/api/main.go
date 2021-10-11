2a89e73a50362ac56038c8ed69593b123b38fe43
Bruno <bruno.moreira@labsit.io>
2021-10-11 16:05:44 -0300

    Atualizacao API

--- DevBook/api/main.go
+++ DevBook/api/main.go
@@ -10,10 +10,19 @@ import (
 
 func main() {
 	config.Carregar()
+
+	fmt.Println(config.StringConexaoBanco)
+
+	fmt.Println("Escutando na porta 5000")
+
+	//Gera um novo Router e encapsula numa variável
 	r := router.Gerar()
 
 	fmt.Println(config.SecretKey)
 
-	fmt.Printf("Escutando na porta %d", config.Porta)
+	//log.Fatal() loga o um Fatal Error
+	//o método ListenAndServe() do objeto http inicializa um servidor HTTP e recebe como parâmetro a porta
+	//onde será executada a requisição e um router ou nil
+	//http.ListenAndServe("<endereço da porta>", <router>)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
